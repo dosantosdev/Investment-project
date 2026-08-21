ea7df32822f66cefb73684424dc45966535264f7
dosantosdev <dosantosdev@gmail.com>
2026-08-21 17:37:10 -0300

    feat: restructure UserInput component for improved layout and organization

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -10,26 +10,38 @@ function UserInput({
 }) {
   return (
     <div id="user-input">
-      <label>Initial Investment</label>
-      <input
-        type="number"
-        value={inputInitialInvestment}
-        onChange={setInitialInvestment}
-      />
-      <label>Annual Investment</label>
-      <input
-        type="number"
-        value={inputAnnualInvestment}
-        onChange={setAnnualInvestment}
-      />
-      <label>Expected Return</label>
-      <input
-        type="number"
-        value={inputExpectedReturn}
-        onChange={setExpectedReturn}
-      />
-      <label>Duration</label>
-      <input type="number" value={inputDuration} onChange={setDuration} />
+      <div className="input-group">
+        <div>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={inputInitialInvestment}
+            onChange={setInitialInvestment}
+          />
+        </div>
+        <div>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={inputAnnualInvestment}
+            onChange={setAnnualInvestment}
+          />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            value={inputExpectedReturn}
+            onChange={setExpectedReturn}
+          />
+        </div>
+        <div>
+          <label>Duration</label>
+          <input type="number" value={inputDuration} onChange={setDuration} />
+        </div>
+      </div>
     </div>
   );
 }
